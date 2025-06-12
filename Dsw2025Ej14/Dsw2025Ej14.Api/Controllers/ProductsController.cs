48cb67e39113fb3e499c9c70d5083b5fc7377660
DeBoeckIg <Ignacio.DeBoeck@alu.frt.utn.edu.ar>
2025-06-12 19:40:58 -0300

    Hasta el punto 7 incompleto

---
+++ Dsw2025Ej14/Dsw2025Ej14.Api/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Dsw2025Ej14.Api.Data;
+using Dsw2025Ej14.Api.Domain;
+using Dsw2025Ej14.Api.Domain.Interfaces;
+
+namespace Dsw2025Ej14.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IPersistencia _persistencia;
+
+        public ProductsController(IPersistencia persistencia)
+        {
+            _persistencia = persistencia;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllActiveProducts()
+        {
+            var products = _persistencia.GetAllActiveProducts();
+
+            if (products == null || !products.Any())
+            {
+                return NoContent(); // 204 si no hay productos activos
+            }
+
+            return Ok(products); // 200 con los productos
+        }
+
+        [HttpGet("{sku}")]
+        public IActionResult GetProductBySku(string sku)
+        {
+            var product = _persistencia.GetBySku(sku);
+
+            if (product == null)
+            {
+                return NotFound(); // 404 si no existe
+            }
+
+            return Ok(product); // 200 con el producto
+        }
+    }
+}
---
+++ Dsw2025Ej14/Dsw2025Ej14.Api/Data/PersistenciaEnMemoria.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using Dsw2025Ej14.Api.Domain.Entities;
+using Dsw2025Ej14.Api.Domain.Interfaces;
+
+namespace Dsw2025Ej14.Api.Data
+{
+    public class PersitenciaEnMemoria : IPersistencia
+    {
+        private List<Product> _products = new();
+
+        public List<Product> GetAllActiveProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product? GetBySku(string sku)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadProducts()
+        {
+            var json = File.ReadAllText("data\\products.json");
+            _products = JsonSerializer.Deserialize<List<Product>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            }) ?? [];
+        }
+    }
+}
